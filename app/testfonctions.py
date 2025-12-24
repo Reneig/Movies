51bb23a2912ac82a020e4626853e4759cb7cd7fd
Reneig <gbodogbezinsourene@gmail.com>
2025-12-24 12:58:26 +0100

    testFonction

---
+++ app/testfonctions.py
@@ -0,0 +1,17 @@
+# %%
+# Importer les modules nécessaires
+from database import SessionLocal
+import Fonctions
+# %%
+# Créer une session
+db = SessionLocal()
+# %%
+# Tester la récupération de films
+movies = Fonctions.get_movies(db, limit=5, genre="Comedy")
+
+for movie in movies:
+    print(f"ID: {movie.movieId}, Titre: {movie.title}, Genres: {movie.genres}")
+
+# %%
+# Fermer la session
+db.close()
